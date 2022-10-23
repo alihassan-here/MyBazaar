f350f39008f8cdd9fe6dee646b6d6753b579f4ae
Ali Hassan <alihassanuog50@gmail.com>
2022-10-23 23:08:57 +0500

    added pagination in categories

--- frontend/src/screens/dashboard/Categories.jsx
+++ frontend/src/screens/dashboard/Categories.jsx
@@ -1,56 +1,90 @@
-import { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import ScreenHeader from '../../components/ScreenHeader';
-import Wrapper from './Wrapper';
-import { clearMessage } from '../../store/reducers/globalReducer';
-import { useGetQuery } from '../../store/services/categoryService';
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import ScreenHeader from "../../components/ScreenHeader";
+import Wrapper from "./Wrapper";
+import { clearMessage } from "../../store/reducers/globalReducer";
+import { useGetQuery } from "../../store/services/categoryService";
 import Spinner from "../../components/Spinner";
+import Pagination from "../../components/Pagination";
 
 const Categories = () => {
-    const { page } = useParams();
-    const { success } = useSelector(state => state.globalReducer);
-    const dispatch = useDispatch();
-    const { data = [], isFetching } = useGetQuery(page ? page : 1);
-    // console.log(data, isLoading);
+  let { page } = useParams();
 
-    useEffect(() => {
-        return () => {
-            dispatch(clearMessage());
-        }
-    }, [dispatch])
-    return (
-        <Wrapper>
-            <ScreenHeader>
-                <Link to="/dashboard/create-category" className='btn-dark'>add category <i className='bi bi-plus'></i></Link>
-            </ScreenHeader>
-            {success && <div className="alert-success">
-                {success}!
-            </div>}
-            {
-                isFetching ? <Spinner /> : data?.categories.length > 0 && <div>
-                    <table className="w-full bg-gray-900 rounded-md">
-                        <thead>
-                            <tr className="border-b border-gray-800 text-left">
-                                <th className="p-3 uppercase text-sm font-medium text-gray-500">name</th>
-                                <th className="p-3 uppercase text-sm font-medium text-gray-500">edit</th>
-                                <th className="p-3 uppercase text-sm font-medium text-gray-500">delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data?.categories?.map(category => (
-                                <tr key={category._id} className="odd:bg-gray-800">
-                                    <td className="p-3 capitalize text-sm font-normal text-gray-400">{category.name}</td>
-                                    <td className="p-3 capitalize text-sm font-normal text-gray-400"><Link to={`/dashboard/update-category/${category._id}`} className="btn btn-warning">edit</Link></td>
-                                    <td className="p-3 capitalize text-sm font-normal text-gray-400"><button className="btn btn-danger" >delete</button></td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            }
-        </Wrapper>
-    )
-}
+  if (!page) {
+    page = 1;
+  }
+  const { success } = useSelector((state) => state.globalReducer);
+  const dispatch = useDispatch();
+  const { data = [], isFetching } = useGetQuery(page);
+  // console.log(data, isLoading);
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearMessage());
+    };
+  }, [dispatch]);
+  return (
+    <Wrapper>
+      <ScreenHeader>
+        <Link to="/dashboard/create-category" className="btn-dark">
+          add category <i className="bi bi-plus"></i>
+        </Link>
+      </ScreenHeader>
+      {success && <div className="alert-success">{success}!</div>}
+      {!isFetching ? (
+        data?.categories.length > 0 && (
+          <>
+            <div>
+              <table className="w-full bg-gray-900 rounded-md">
+                <thead>
+                  <tr className="border-b border-gray-800 text-left">
+                    <th className="p-3 uppercase text-sm font-medium text-gray-500">
+                      name
+                    </th>
+                    <th className="p-3 uppercase text-sm font-medium text-gray-500">
+                      edit
+                    </th>
+                    <th className="p-3 uppercase text-sm font-medium text-gray-500">
+                      delete
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data?.categories?.map((category) => (
+                    <tr key={category._id} className="odd:bg-gray-800">
+                      <td className="p-3 capitalize text-sm font-normal text-gray-400">
+                        {category.name}
+                      </td>
+                      <td className="p-3 capitalize text-sm font-normal text-gray-400">
+                        <Link
+                          to={`/dashboard/update-category/${category._id}`}
+                          className="btn btn-warning"
+                        >
+                          edit
+                        </Link>
+                      </td>
+                      <td className="p-3 capitalize text-sm font-normal text-gray-400">
+                        <button className="btn btn-danger">delete</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <Pagination
+              page={parseInt(page)}
+              perPage={data.perPage}
+              count={data.count}
+              path="dashboard/categories"
+            />
+          </>
+        )
+      ) : (
+        <Spinner />
+      )}
+    </Wrapper>
+  );
+};
 
 export default Categories;
--- frontend/src/store/services/categoryService.js
+++ frontend/src/store/services/categoryService.js
@@ -1,43 +1,42 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const categoryService = createApi({
-    reducerPath: "category",
-    tagTypes: "categories",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api/",
-        prepareHeaders: (headers, state) => {
-            const allReducers = state.getState();
-            const token = allReducers?.authReducer?.adminToken;
-            headers.set('Authorization', token ? 'Bearer ' + token : "");
-            return headers;
-        }
-    }),
-    endpoints: (builder) => {
-        return {
-            create: builder.mutation({
-                query: (name) => {
-                    return {
-                        url: "create-category",
-                        method: "POST",
-                        body: name
-                    }
-                },
-                invalidatesTags: ["categories"]
-            }),
-            get: builder.query({
-                query: (page) => {
-                    return {
-                        url: `categories/${page}`,
-                        method: "GET",
-                    }
-                },
-                providesTags: ["categories"],
-            })
-        }
-    }
+  reducerPath: "category",
+  tagTypes: "categories",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3060/api/",
+    prepareHeaders: (headers, state) => {
+      const allReducers = state.getState();
+      const token = allReducers?.authReducer?.adminToken;
+      headers.set("Authorization", token ? "Bearer " + token : "");
+      return headers;
+    },
+  }),
+  endpoints: (builder) => {
+    return {
+      create: builder.mutation({
+        query: (name) => {
+          return {
+            url: "create-category",
+            method: "POST",
+            body: name,
+          };
+        },
+        invalidatesTags: ["categories"],
+      }),
+      get: builder.query({
+        query: (page) => {
+          return {
+            url: `categories/${page}`,
+            method: "GET",
+          };
+        },
+        providesTags: ["categories"],
+      }),
+    };
+  },
 });
 
-export const { useCreateMutation, useGetQuery } = categoryService
-
+export const { useCreateMutation, useGetQuery } = categoryService;
 
 export default categoryService;
