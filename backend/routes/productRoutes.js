0bebe469e9ab3c55268d1ce43d109ba5dbd101d4
Ali Hassan <alihassanuog50@gmail.com>
2023-01-02 15:58:28 +0500

    upload images backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Authorization = require("../services/Authorization");
 const productController = require("../controllers/productController");
-const productValidations = require("../validations/productValidations");
 
 router.post(
   "/create-product",
-  [Authorization.authorized, productValidations],
+  [Authorization.authorized],
   productController.create
 );
 
