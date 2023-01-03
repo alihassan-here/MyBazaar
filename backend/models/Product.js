fad422b0ba0bd0d09263be0b6534be0c38d7bbe3
Ali Hassan <alihassanuog50@gmail.com>
2023-01-03 16:33:56 +0500

    save product to db

---
+++ backend/models/Product.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      required: true,
+      type: Number,
+    },
+    stock: {
+      required: true,
+      type: Number,
+    },
+    category: {
+      required: true,
+      type: String,
+    },
+    colors: {
+      type: [Map],
+    },
+    sizes: {
+      type: [Map],
+    },
+    price: {
+      required: true,
+      type: Number,
+    },
+    image1: {
+      required: true,
+      type: String,
+    },
+    image2: {
+      required: true,
+      type: String,
+    },
+    image3: {
+      required: true,
+      type: String,
+    },
+    description: {
+      required: true,
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
